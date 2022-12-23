8b7d921bb2f581650fbc88205d1183d0ad32e07e
Artem007 <artem.zagrebelny@yandex.ru>
2022-12-23 10:20:58 +0300

    Added some tasks of the second seminar

---
+++ Семинар 3 С#/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09 
+//A (7,-5); B (1,-1) -> 7,21
+
+// для первой прямой
+Console.WriteLine("Введите х-координату первой прямой: ");
+double X1 = Convert.ToInt(Console.ReadLine());
+Console.WriteLine("Введите Y-координату первой прямой: ");
+double Y1 = Convert.ToInt(Console.ReadLine);
+
+//для второй прямой
+Console.WriteLine("Введите х-координату второй прямой: ");
+double X2 = Convert.ToInt(Console.ReadLine());
+Console.WriteLine("Введите Y-координату второй прямой: ");
+double Y2 = Convert.ToInt(Console.ReadLine);
+
+// расстояние
+double rast
+
---
+++ Семинар 3 С#/Задание 22/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N) и выдает таблицу квадратов чмсел
+// от 1 до N
+
+Console.WriteLine("Введите число");
+double number = Convert.ToInt32(Console.ReadLine());
+double i = 1;
+
+while(i <= number)
+{
+    double square = Math.Pow(i, 2);
+    Console.Write(square + " ");
+    i++ ;
+}

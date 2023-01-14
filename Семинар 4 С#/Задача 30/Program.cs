35a51b16d8a8efb8fa581b15900e9812c777bad6
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-14 04:21:09 +0300

    Added new variant ex 30

--- Семинар 4 С#/Задача 30/Program.cs
+++ Семинар 4 С#/Задача 30/Program.cs
@@ -6,9 +6,9 @@ int[] array = new int[8];
 for(int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(0,2);
-    Console.Write(array[i] + " ");
+    //Console.Write(array[i] + " ");
 }
-
+Console.WriteLine('[' + string.Join(", ", array) + ']');
 // for(int i = 0; i < array.Length; i++)
 //{
 //   Console.Write(array[i] + " ");

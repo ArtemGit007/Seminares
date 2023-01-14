aab60211b3d17b926ef7335bb137574fd164f907
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-14 03:29:19 +0300

    Added another variant of ex.26

---
+++ Семинар 4 С#/Задача 26/Задача 26(1)/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(" Введите число: ");
+string number = Console.ReadLine();
+
+if(int.TryParse(number, out int A))
+{
+ 
+    Console.WriteLine(number.Length);
+}
+else
+{
+    Console.WriteLine(" Ошибка ");
+}
+
+
+

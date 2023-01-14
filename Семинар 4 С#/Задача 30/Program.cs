a247afe8efc504093cda7e0c82131c475d8df804
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-14 04:05:20 +0300

    Added anotheк variant ex. 30

--- Семинар 4 С#/Задача 30/Program.cs
+++ Семинар 4 С#/Задача 30/Program.cs
@@ -6,9 +6,10 @@ int[] array = new int[8];
 for(int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(0,2);
-}
-
-for(int i = 0; i < array.Length; i++)
-{
     Console.Write(array[i] + " ");
 }
+
+// for(int i = 0; i < array.Length; i++)
+//{
+//   Console.Write(array[i] + " ");
+//}

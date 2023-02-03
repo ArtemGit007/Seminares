6de5521a0ef5bacebcc54de64640a8cb482be150
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-03 21:52:54 +0300

    Added ex.63

---
+++ Семинар 9 C#/Задача 63/Program.cs
@@ -0,0 +1,18 @@
+﻿ //Задача 63. Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до  N.
+
+void PrintNumbers(int number)
+{
+   if(number == 0)
+   {
+       return;
+    }
+     PrintNumbers(number - 1);
+     Console.Write(number + " "); 
+
+}
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(N-1);
+
